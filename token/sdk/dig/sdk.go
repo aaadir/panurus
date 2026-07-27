hyperledger-labs/fabric-token-sdk

limit the number of tokens a transaction retrieves and locks
Signed-off-by: Hayim.Shaul@ibm.com <hayimsha@fhe03.vpc.cloud9.ibm.com>

token/sdk/dig/sdk.go
@@ -139,8 +139,14 @@ func (p *SDK) Install() error {
 		p.Container().Provide(digutils.Identity[*ftscore.TMSProvider](), dig.As(new(ftsdriver.TokenManagerServiceProvider))),
 		p.Container().Provide(tms.NewPostInitializer),
 
-		p.Container().Provide(func(ttxStoreServiceManager ttxdb.StoreServiceManager) *network2.LockerProvider {
-			return network2.NewLockerProvider(ttxStoreServiceManager, 2*time.Second, 5*time.Minute)
+		p.Container().Provide(func(ttxStoreServiceManager ttxdb.StoreServiceManager, configService fscconfig.ConfigService) *network2.LockerProvider {
+			cfg, err := config.New(configService)
+			if err != nil {
+				logging.MustGetLogger().Errorf("error getting selector config for locker, using defaults: %s", err.Error())
+				return network2.NewLockerProvider(ttxStoreServiceManager, 2*time.Second, 5*time.Minute)
+			}
+			limits := cfg.GetLimits()
+			return network2.NewLockerProviderWithLimits(ttxStoreServiceManager, 2*time.Second, 5*time.Minute, limits.MaxLocksPerTransaction)
 		}, dig.As(new(simple.LockerProvider))),
 		p.Container().Provide(selectorProviders[sdriver.Driver(p.ConfigService().GetString("token.selector.driver"))], dig.As(new(token.SelectorManagerProvider))),
 		p.Container().Provide(network2.NewCertificationClientProvider, dig.As(new(token.CertificationClientProvider))),

aaadir/panurus

token/sdk/dig/sdk.go
@@ -149,8 +149,16 @@ func (p *SDK) Install() error {
 		p.Container().Provide(digutils.Identity[*ftscore.TMSProvider](), dig.As(new(ftsdriver.TokenManagerServiceProvider))),
 		p.Container().Provide(tms.NewPostInitializer),
 
-		p.Container().Provide(func(ttxStoreServiceManager ttxdb.StoreServiceManager) *network2.LockerProvider {
-			return network2.NewLockerProvider(ttxStoreServiceManager, 2*time.Second, 5*time.Minute)
+		p.Container().Provide(func(ttxStoreServiceManager ttxdb.StoreServiceManager, configService driver.ConfigService) *network2.LockerProvider {
+			cfg, err := config.New(configService)
+			if err != nil {
+				logging.MustGetLogger().Errorf("error getting selector config for locker, using defaults: %s", err.Error())
+
+				return network2.NewLockerProvider(ttxStoreServiceManager, 2*time.Second, 5*time.Minute)
+			}
+			limits := cfg.GetLimits()
+
+			return network2.NewLockerProviderWithLimits(ttxStoreServiceManager, 2*time.Second, 5*time.Minute, limits.MaxLocksPerTransaction)
 		}, dig.As(new(simple.LockerProvider))),
 		p.Container().Provide(selectorProviders[sdriver.Driver(p.ConfigService().GetString("token.selector.driver"))], dig.As(new(token.SelectorManagerProvider))),
 		p.Container().Provide(network2.NewCertificationClientProvider, dig.As(new(token.CertificationClientProvider))),